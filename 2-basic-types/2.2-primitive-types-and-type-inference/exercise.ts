7de36bb0bf790abfff23c5b23072c385319e25cb
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-05-24 19:26:35 +0300

    solve basic types section exercies

--- 2-basic-types/2.2-primitive-types-and-type-inference/exercise.ts
+++ 2-basic-types/2.2-primitive-types-and-type-inference/exercise.ts
@@ -1,11 +1,16 @@
 // Exercise: Declare variables with different primitive types in TypeScript
 
 // 1. Declare a variable named `planet` of type string and assign it the value "Earth".
+let planet: string = "Earth";
 
 // 2. Declare a variable named `isRaining` of type boolean and assign it the value false.
+let isRaining = false;
 
 // 3. Declare a const named `pi` of type number and assign it the value 3.14.
+const pi: number = 3.14;
 
 // 4. Assign a value of the wrong type to one of variables above. What happens?
+isRaining = "abc" // string is not assignable to type boolean
 
 // 5. Remove the explicit type annotations above, then hover over each variable's name. Does anything change?
+// we still see the error
--- 2-basic-types/2.3-typing-arrays-and-tuples/exercise.ts
+++ 2-basic-types/2.3-typing-arrays-and-tuples/exercise.ts
@@ -1,12 +1,15 @@
 // Exercise: Typing Arrays in TypeScript
 
 // 1. Declare an array of numbers called `ages` and initialize it with the values 25, 30, 35, and 40.
+let ages: number[] = [25, 30, 35, 40];
 
 // 2. Declare an array of strings called `names` and initialize it with the values "Alice", "Bob", and
 // "Charlie".
+let names: string[] = ["Alice", "Bob", "Charlie"];
 
 // 3. Declare a tuple called `product` that contains a string and a number.
 // Initialize it with the values "Boat" and 25.
+let product: [string, number] = ["Boat", 25];
 
 // 4. Declare an array of tuples called `coordinates` where each tuple contains two numbers (x and y).
 //    Initialize it with the following values:
@@ -14,6 +17,14 @@
 //    - (30, 40)
 //    - (50, 60)
 
+let coordinates: [number, number][] = 
+    [
+        [10, 20],
+        [30, 40],
+        [50, 60]
+    ];
+
 // 5. Use the spread syntax to combine the two array types below into a new array type called `combined`.
 type Continents = ["Europe", "Africa"];
 type Oceans = ["Pacific", "Atlantic"];
+type combined = [...Oceans, ...Continents];
