0d2c2557602a1c73f40f60c1c6bb95f6e5a38504
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-05-25 15:53:36 +0300

    solve typing fundamential section

--- 3-typing-fundamentals/3.1-discriminated-unions/exercise.ts
+++ 3-typing-fundamentals/3.1-discriminated-unions/exercise.ts
@@ -6,9 +6,32 @@
 // The function should return "A car with X doors." for car shapes,
 // "A truck with a payload capacity of Y." for truck shapes
 
-// Test cases
-// const car: Vehicle = { kind: "car", numberOfDoors: 4 };
-// const truck: Vehicle = { kind: "truck", payloadCapacity: 2000 };
+interface Car {
+    kind: "Car";
+    numberOfDoors: number;
+}
 
-// console.log(describeVehicle(car)); // Should print the description of the car
-// console.log(describeVehicle(truck)); // Should print the description of the truck
+interface Truck {
+    kind: "Truck";
+    payloadCapacity: number;
+}
+
+type Vehicle = Car | Truck;
+
+function describeVehicle(vehicle: Vehicle) {
+    switch (vehicle.kind) {
+        case "Car":
+            return `A car with ${vehicle.numberOfDoors} doors`;
+        case "Truck":
+            return `A truck with payload capacity of ${vehicle.payloadCapacity}`;
+    }
+}
+
+
+
+// Test cases
+const car: Vehicle = { kind: "Car", numberOfDoors: 4 };
+const truck: Vehicle = { kind: "Truck", payloadCapacity: 2000 };
+
+console.log(describeVehicle(car)); // Should print the description of the car
+console.log(describeVehicle(truck)); // Should print the description of the truck
--- 3-typing-fundamentals/3.3-optional-fields-on-objects/exercise.ts
+++ 3-typing-fundamentals/3.3-optional-fields-on-objects/exercise.ts
@@ -1,3 +1,17 @@
 // Define an interface for a User with the following fields:
 // - id (number)
 // - email (optional string)
+
+interface User {
+    id: number;
+    email?: string;
+}
+
+const user: User = {
+    id: 1,
+    email: "example@example.com",
+}
+
+const userWithoutEmail: User = {
+    id: 1
+}
