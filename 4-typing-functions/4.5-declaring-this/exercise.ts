acdd566a55e5c946f8460acd104bc7ce75998a39
Ahmed Fattouh <120097440+Tenno641@users.noreply.github.com>
2026-05-26 14:15:09 +0300

    typing functions section solution

--- 4-typing-functions/4.5-declaring-this/exercise.ts
+++ 4-typing-functions/4.5-declaring-this/exercise.ts
@@ -4,13 +4,31 @@
 // Create an object literal called `user` with a property `username` and a method `introduce`.
 // The `introduce` method should return an introduction message that includes the
 // `username` property using `this`.
+const user = {
+    username: "username",
+    introduce(): string {
+        return `Welcome to ${this.username}!`;
+    }
+}
 
 // Question 2:
 // Create a class `Counter` with a property `count` initialized to 0 and a method `increment`.
 // The `increment` method should increase the `count` property by 1 using `this`.
+class Counter {
+    private count: number = 0;
+    increment() {
+        this.count++;  
+    } 
+}
 
 // Question 3:
 // Create a function `sayHello` that logs a greeting message for the person object below using `this`.
 // Use the `call` method to invoke the `sayHello` function with the `person` object.
 
+function sayHello(this: { username: string }) {
+    console.log(`Hello, ${this.username}`)
+}
+
 const person = { username: "Alice" };
+
+sayHello.call(person);
