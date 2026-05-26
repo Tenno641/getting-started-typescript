135e1562de8c60b7091010499a628afa67f88d36
Ahmed Fattouh <ahmedfotoh2003@gmail.com>
2026-05-26 14:13:49 +0300

    typing functions section solution

--- 4-typing-functions/4.3-function-overloads/exercise.ts
+++ 4-typing-functions/4.3-function-overloads/exercise.ts
@@ -1,6 +1,13 @@
 // Exercise 1: Create function overloads for a function named `getLength`
 // that can take either a string or an array and return their length.
 
+function getLength(value: string): number;
+function getLength(value: any[]): number;
+
+function getLength(value: any[] | string): number {
+    return value.length;
+}
+
 // Test your implementation:
-// getLength("TypeScript"); // 10
-// getLength([1, 2, 3, 4, 5]); // 5
+getLength("TypeScript"); // 10
+getLength([1, 2, 3, 4, 5]); // 5
